5a2a735b076bd20e762d84eade487f84d19917a7
mnadon <63380957+mnadon@users.noreply.github.com>
2025-01-03 12:16:41 -0500

    install-bundle.ps1

---
+++ install-bundle.ps1
@@ -0,0 +1,11 @@
+# Install Tracker Software PDF-XChange Editor
+winget install TrackerSoftware.PDF-XChangeEditor -h
+
+# Install Google Earth Pro
+winget install Google.EarthPro -h
+
+# Uninstall Microsoft Teams Free
+winget uninstall Microsoft.Teams.Free -h
+
+# Install Microsoft Teams
+winget install Microsoft.Teams -h
